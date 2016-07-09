a16c8af2cd1dcc2cd59a3404236cf91a832c4060
anilkumarthalikoti <anil2k12@gmail.com>
2016-07-09 10:40:40 +0530

    added baseurl

--- services/AjaxServices.js
+++ services/AjaxServices.js
@@ -4,9 +4,9 @@ define(['application-configuration'], function (app) {
     app.register.service('ajaxService', ['$http', 'blockUI', function ($http, blockUI) {
 
         // setting timeout of 1 second to simulate a busy server.
-
+var baseurl="http://localhost:8080/events/";
         this.AjaxPost = function (data, route, successFunction, errorFunction) {
-            
+            route=baseurl+route;
             blockUI.start();
             setTimeout(function () {
                 $http.post(route, data).success(function (response, status, headers, config) {
@@ -22,6 +22,7 @@ define(['application-configuration'], function (app) {
         }
 
         this.AjaxPostWithNoAuthenication = function (data, route, successFunction, errorFunction) {
+		route=baseurl+route;
             blockUI.start();
             setTimeout(function () {
                 $http.post(route, data).success(function (response, status, headers, config) {
@@ -37,6 +38,7 @@ define(['application-configuration'], function (app) {
 
         this.AjaxGet = function (route, successFunction, errorFunction) {
             blockUI.start();
+			route=baseurl+route;
             setTimeout(function () {
                 $http({ method: 'GET', url: route }).success(function (response, status, headers, config) {
                     blockUI.stop();
@@ -52,6 +54,7 @@ define(['application-configuration'], function (app) {
 
         this.AjaxGetWithData = function (data, route, successFunction, errorFunction) {
             blockUI.start();
+			route=baseurl+route;
             setTimeout(function () {
                 $http({ method: 'GET', url: route, params: data }).success(function (response, status, headers, config) {
                     blockUI.stop();
@@ -66,7 +69,8 @@ define(['application-configuration'], function (app) {
         }
 
 
-        this.AjaxGetWithNoBlock = function (data, route, successFunction, errorFunction) {            
+        this.AjaxGetWithNoBlock = function (data, route, successFunction, errorFunction) {     
+route=baseurl+route;		
             setTimeout(function () {
                 $http({ method: 'GET', url: route, params: data }).success(function (response, status, headers, config) {                 
                     successFunction(response, status);
